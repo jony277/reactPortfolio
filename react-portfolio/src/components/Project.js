6d0241eb7657a35030f756dfaacb009d09289917
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-09-18 15:16:48 -0400

    added folder structure and code for index.js

---
+++ react-portfolio/src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project = () => {
+    return (
+        <div>
+            <h2>Project Title</h2>
+            
+        </div>
+    );
+};
+
+export default Project;
